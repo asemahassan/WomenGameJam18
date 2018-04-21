dbcf3087b23154572668425c82e626cc9d051f0e
ru_ka <ru_ka@uni-bremen.de>
2018-04-21 21:11:03 +0200

    story scene

---
+++ Octopus-Love-Remake/Assets/Remake_Platformer/Scripts/Puzzles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzles : MonoBehaviour {
+
+
+	public GameObject platform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void PlatformBreaks () {
+		
+	}
+
+
+
+	void CreateRandomPuzzle () {
+	}
+}
