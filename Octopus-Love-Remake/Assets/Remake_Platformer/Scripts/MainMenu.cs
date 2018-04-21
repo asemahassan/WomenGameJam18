48b4e839f4af15efa47ab2e605048dfce8271eec
shrutisub <shruti.subramanian@st.ovgu.de>
2018-04-21 09:15:22 +0200

    main menu
    main menu

---
+++ Octopus-Love-Remake/Assets/Remake_Platformer/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+  
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1); //remake   
+    }
+
+    void Update()
+    {
+    }
+}
+
