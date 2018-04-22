4f3dcf46f8d5e20505dd57f58471b098f00f372e
Asema Hassan <asema.hassan@gmail.com>
2018-04-22 21:02:21 +0200

    update

---
+++ Octopus-Love-Remake/Assets/Complete.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Complete : MonoBehaviour
+{
+
+	[SerializeField]
+	AudioClip heartClip = null;
+	GameObject particleSystem = null;
+
+	[SerializeField]
+	GameObject octoMale = null;
+
+	public bool hasComplete = false;
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		// If the player enters the trigger zone...
+		if (other.tag == "Player" && !hasComplete) {
+
+
+			//enable octopus male
+			if (octoMale != null) {
+				octoMale.SetActive (true);
+				particleSystem = octoMale.transform.Find ("HeartParticles").gameObject;
+			}
+
+			//play heart particles
+			if (particleSystem != null)
+				particleSystem.GetComponent <ParticleSystem> ().Play ();
+			//play heart sound
+			AudioSource.PlayClipAtPoint (heartClip, this.transform.position);
+			hasComplete = false;
+		}
+	}
+
+}
---
+++ Octopus-Love-Remake/Assets/SoundPlay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlay : MonoBehaviour
+{
+
+	[SerializeField]
+	AudioSource firstMs = null;
+
+	[SerializeField]
+	AudioSource secondMs = null;
+	public float timeOutForFirtMusic = 5;
+
+	[SerializeField]
+	Complete completeCheck = null;
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine ((playSecondTheme ()));
+	}
+
+	IEnumerator playSecondTheme ()
+	{
+
+		yield return new WaitForSeconds (timeOutForFirtMusic);
+		firstMs.Stop ();
+		firstMs.enabled = false;
+		secondMs.enabled = true;
+		secondMs.Play ();
+	}
+
+
+	void Update ()
+	{
+		if (completeCheck.hasComplete) {
+			if (secondMs.enabled == true) {
+				secondMs.enabled = false;
+				secondMs.Stop ();
+			}
+		
+			if (firstMs.enabled == true) {
+				firstMs.Stop ();
+				firstMs.enabled = false;
+			}
+		}
+	}
+}
